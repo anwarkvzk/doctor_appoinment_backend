340803e7abaa7f5fa855e1706be830248daee6c0
anwarkvzk <anvarvzk521@gmail.com>
2023-10-08 16:51:53 +0530

    admin Functionality added

--- models/userModels.js
+++ models/userModels.js
@@ -1,20 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is require']
-    },
-    email:{
-        type: String,
-        required: [true, 'Email is required']
-    },
-    password:{
-        type: String,
-        required: [true, 'Password is required']
-    }
-})
+  name: {
+    type: String,
+    required: [true, "Name is require"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isDoctor: {
+    type: Boolean,
+    default: false,
+  },
+  notification: {
+    type: Array,
+    default: [],
+  },
+  seennotification: {
+    type: Array,
+    default: [],
+  },
+});
 
-const userModel = mongoose.model('users', userSchema)
+const userModel = mongoose.model("users", userSchema);
 
-module.exports = userModel
+module.exports = userModel;
