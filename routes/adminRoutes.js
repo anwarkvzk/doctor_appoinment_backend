ed680d1e372efceefb3f4aefd37e85d5cfaae6af
anwarkvzk <anvarvzk521@gmail.com>
2023-10-10 23:39:26 +0530

    addmin Account approvel status controller added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const {
   getAllUsersController,
   getAllDoctorsController,
+  changeAccountStatusController,
 } = require("../controllers/adminController");
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.get("/getAllUsers", authMiddleware, getAllUsersController);
 
 //get Method || Doctors
 router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
+
+//Post Account Status
+router.post('/changeAccountStatus', authMiddleware, changeAccountStatusController)
 
 module.exports = router;
